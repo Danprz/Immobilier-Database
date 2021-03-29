66ddebb72354a539801bf54ed642bed1a0baee90
Dani PEREZ <danperezm93@gmail.com>
2021-03-29 09:47:43 +0200

    Etude de la saisonnalité du marche immobilier autour de Fougères

--- Query_2.sql
+++ Query_2.sql
@@ -2,7 +2,7 @@ SELECT
 	EXTRACT(month FROM date_vente) AS mois_vente,
     COUNT(id) as total_des_maisons
 FROM statuts
-group by mois_vente
+GROUP BY mois_vente
 ORDER BY total_des_maisons DESC
 ;
 
